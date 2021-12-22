f39c170846aea3ac73c66accc989d7861f67fae5
Cauanorl <rodriguescauan701@gmail.com>
2021-12-22 19:04:39 -0300

    Iniciando o primeiro projeto com mongo db

---
+++ index.js
@@ -0,0 +1,10 @@
+// CRUD --- Create Read Update Delete
+// As quatros operações basicas de um banco de dados
+
+const express = require('express')
+const path = require('path')
+
+
+const app = express()
+
+
